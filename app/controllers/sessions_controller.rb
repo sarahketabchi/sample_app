f5cab37c21d0516c5a0096f5e37c888e859c836c
Sarah Ketabchi <sarahketabchi@gmail.com>
2013-02-04 10:03:18 -0800

    Completed invalid signin

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+
+		if user && user.authenticate(params[:session][:password])
+			# Sign in the user and redirect to the user's show page
+		else
+			# Create an error message and re-render the signin form.
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  #get "static_pages/home"
   root to: 'static_pages#home'
   # also delete public/index.html for this root to: to work!
 
-  #get "static_pages/help"
   match '/help', to: 'static_pages#help'
 
-  #get "static_pages/about"
   match '/about', to: 'static_pages#about'
 
-  #get "static_pages/contact"
   match '/contact', to: 'static_pages#contact'
 
   match '/signup', to: 'users#new'
+  match '/signin/', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
