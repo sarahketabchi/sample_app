8111d23b995b93b5d0c075167a2612338978edef
Sarah Ketabchi <sarahketabchi@gmail.com>
2013-02-04 15:14:46 -0800

    Created index for users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -40,9 +40,12 @@ describe "Authentication" do
 			before { sign_in user }
 
 			it { should have_selector('title', text: user.name) }
+
+			it { should have_link('Users', href: users_path) }
 			it { should have_link('Settings', href: edit_user_path(user)) }
 			it { should have_link('Profile', href: user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
+			
 			it { should_not have_link('Sign in', href: signin_path) }
 		end
 	end
@@ -79,6 +82,11 @@ describe "Authentication" do
 					# use put instead of visit to force visit to update action directly
 					before { put user_path(user) }
 					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_selector('title', text: 'Sign in') }
 				end
 			end
 		end
