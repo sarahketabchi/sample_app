9b2aa19dadfab19849d007a669ea86d8206e4a38
App Academy <app-academy@sisley.att.net>
2013-02-08 18:27:19 -0800

    the reign of tab terror is over!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,25 @@
 class SessionsController < ApplicationController
-	def new
-	end
+  def new
+  end
 
-	def create
-		user = User.find_by_email(params[:session][:email].downcase)
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
 
-		if user && user.authenticate(params[:session][:password])
-			# Sign in the user and redirect to the user's show page
-			# user sign_in method in sessions helper module
-			sign_in user
-			redirect_back_or user
-		else
-			# Create an error message and re-render the signin form.
-			flash.now[:error] = 'Invalid email/password combination'
-			render 'new'
-		end
-	end
+    if user && user.authenticate(params[:session][:password])
+      # Sign in the user and redirect to the user's show page
+      # user sign_in method in sessions helper module
+      sign_in user
+      redirect_back_or user
+    else
+      # Create an error message and re-render the signin form.
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
-	def destroy
-		# sign_out is in sessions helper module
-		sign_out
-		redirect_to root_url
-	end
+  def destroy
+    # sign_out is in sessions helper module
+    sign_out
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,25 @@ class UsersController < ApplicationController
   before_filter :admin_user, only: :destroy
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
-  	@user = User.new(params[:user])
+    @user = User.new(params[:user])
 
-  	if @user.save
+    if @user.save
       # sign in automatically when a user signs up
       sign_in @user
       
-  		flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def edit
